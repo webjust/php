0ce5270dbd4fa027f22b52b26887e4fd5906de83
webjust <604854119@qq.com>
2017-05-08 19:06:48 +0800

    链式操作

--- framework/index.php
+++ framework/index.php
@@ -4,39 +4,6 @@ define('BASEDIR', __DIR__);
 include BASEDIR . '/Think/Loder.php';
 spl_autoload_register('\\Think\\Loder::autoload');
 
-// 入栈
-$stack = new SplStack();
-$stack->push("data1<br/>");
-$stack->push("data2<br/>");
-
-echo $stack->pop();
-echo $stack->pop();
-
-echo "<hr/>";
-
-// 队列
-$queue = new SplQueue();
-$queue->enqueue("data1<br/>");
-$queue->enqueue("data2<br/>");
-
-echo $queue->dequeue();
-echo $queue->dequeue();
-
-echo "<hr/>";
-
-// 堆
-$heap = new SplMinHeap();
-$heap->insert("data1<br/>");
-$heap->insert("data2<br/>");
-
-echo $heap->extract();
-echo $heap->extract();
-
-echo "<hr />";
-
-// 固定长度的数组
-$array = new SplFixedArray(10);
-$array[1] = 'num 1';
-$array[5] = 'num 5';
-
-var_dump($array);
+$db = new \Think\Database();
+$res = $db->where('id=1')->where('name=zhangsan')->order('id desc')->limit(10);
+var_dump($res);
